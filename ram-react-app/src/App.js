6ed14c0c234f172b56c81648c155cce7f0cb3790
Jyotiram-k <96097345+Jyotiram-k@users.noreply.github.com>
2021-12-16 19:23:09 +0530

    Sample_React_Project

---
+++ ram-react-app/src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        <p>
+          Click here to navigate to Smiles Central
+        </p>
+        <a
+          className="App-link"
+          href="https://smilescentral.happiestminds.com/home"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Smiles Central Home
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
